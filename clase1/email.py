1e0df14fccdffcb005139f4d2168d3101cb7ebd2
Pablonio <pblvmunoz@gmail.com>
2023-10-02 11:03:00 -0400

    commit1

---
+++ clase1/email.py
@@ -0,0 +1,7 @@
+correo = ['@','.']
+correo_ingresado = input("Ingresa un correo electrónico: ")
+
+if (correo == correo_ingresado):
+    print("El correo electrónico es válido.")
+else:
+    print("El correo electrónico no es válido.")
